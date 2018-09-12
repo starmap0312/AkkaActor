0dcdfeeb9e3894d21534bd35e2f5132560205f9f
kuanyu <starmap0312@gmail.com>
2018-09-12 22:59:17 +0800

    add more

--- src/main/scala/akka_stream/SimpleWebClient.scala
+++ src/main/scala/akka_stream/SimpleWebClient.scala
@@ -1,10 +1,12 @@
 package akka_stream
 
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Framing, Tcp}
 import akka.util.ByteString
 
+import scala.concurrent.Future
 import scala.io.StdIn
 
 // write a simple socket client with Akka Streams
@@ -12,8 +14,9 @@ object SimpleWebClient extends App {
   implicit val system = ActorSystem("SimpleWebClient")
   implicit val materializer = ActorMaterializer()
 
-  val outgoingConnectionFlow = Tcp().outgoingConnection("127.0.0.1", 9000)
-  val flow = Flow[ByteString]
+  val outgoingConnectionFlow: Flow[ByteString, ByteString, Future[Tcp.OutgoingConnection]] =
+    Tcp().outgoingConnection("127.0.0.1", 9000)
+  val flow: Flow[ByteString, ByteString, NotUsed] = Flow[ByteString]
     .via(
       Framing.delimiter(
         ByteString("\n"),
@@ -26,7 +29,7 @@ object SimpleWebClient extends App {
     .map(_ => StdIn.readLine("> "))
     .map(_ + "\n")
     .map(ByteString(_))
-  outgoingConnectionFlow.join(flow).run()
+  val matValue: Future[Tcp.OutgoingConnection] = outgoingConnectionFlow.join(flow).run()
   // Join this Flow to another Flow, by cross connecting the inputs and outputs
 
   //  in contrast to the simple web server, we don't have to manage the incoming connections anymore
