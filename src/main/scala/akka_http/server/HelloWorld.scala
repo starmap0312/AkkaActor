eb2ab0ec71bfca9414dbe4a1e3b6f8ce9048364c
kuanyu <starmap0312@gmail.com>
2018-09-14 18:33:34 +0800

    add more

--- src/main/scala/akka_http/server/HelloWorld.scala
+++ src/main/scala/akka_http/server/HelloWorld.scala
@@ -2,7 +2,7 @@ package akka_http.server
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.model.{HttpMethods, HttpRequest, HttpResponse, Uri}
 import akka.stream.ActorMaterializer
 
 import scala.concurrent.Future
@@ -15,7 +15,13 @@ object HelloWorld extends App {
   implicit val materializer = ActorMaterializer()
 
   def handler(request: HttpRequest) = {
-    Future.successful(HttpResponse(entity = "Hello World"))
+    // ex1. always return "Hello World" regardless of the HttpRequest, i.e. curl http://localhost:9000
+    //Future.successful(HttpResponse(entity = "Hello World"))
+    // ex2. if request matches the following path, i.e. curl http://localhost:9000/abc
+    request match {
+      case HttpRequest(HttpMethods.GET, Uri.Path("/abc"), _, _, _) =>
+        Future.successful(HttpResponse(entity = "Hello World"))
+    }
   }
   Http().bindAndHandleAsync(handler, "localhost", 9000).
     onComplete {
