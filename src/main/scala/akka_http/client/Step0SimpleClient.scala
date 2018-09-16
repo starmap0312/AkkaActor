9c5a7ef02cca5e9a0061d61058b30c9356c45375
kuanyu <starmap0312@gmail.com>
2018-09-16 20:00:41 +0800

    add more

---
+++ src/main/scala/akka_http/client/Step0SimpleClient.scala
@@ -0,0 +1,34 @@
+package akka_http.client
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.coding.Gzip
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.stream.scaladsl.Source
+
+import scala.concurrent.Future
+import scala.io.StdIn
+import scala.util.{Failure, Success}
+
+object Step0SimpleClient extends App {
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+  import system.dispatcher
+
+  val request: HttpRequest = HttpRequest(uri = "http://localhost:9000/abc")
+  val responseFuture: Future[HttpResponse] = Http().singleRequest(request)
+  val sourceFuture: Future[Source[String, Any]] = responseFuture.map { response =>
+    response.entity.dataBytes. // Source[ByteString, Any]
+      //via(Gzip.decoderFlow). // conversion flow for decoding Gzipped content bytestring
+      map(_.utf8String)        // Source[String, Any]: converting ByteString to (utf8) String
+  }
+  sourceFuture onComplete {
+    case Success(source) => source.runForeach(println)
+    case Failure(ex) =>
+      ex.printStackTrace()
+      system.terminate()
+  }
+  StdIn.readLine()
+  system.terminate()
+}
---
+++ src/main/scala/akka_http/client/Step1StreamServer.scala
@@ -0,0 +1,53 @@
+package akka_http.client
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
+
+import scala.concurrent.Future
+import scala.io.StdIn
+import scala.util.{Failure, Success}
+
+object Step1StreamServer extends App {
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+  import system.dispatcher
+
+  def sourceFuture: Future[Source[String, Any]] = {
+    val request: HttpRequest = HttpRequest(uri = "http://localhost:9000/chunked")
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(request)
+    val future: Future[Source[String, Any]] = responseFuture.map { response =>
+      response.entity.dataBytes. // Source[ByteString, Any]
+        map(_.utf8String) // Source[String, Any]
+    }
+    future
+  }
+
+  val route: Route =
+    get {
+      onSuccess(sourceFuture) { source =>
+        complete {
+          HttpResponse(
+            entity = HttpEntity.Chunked.fromData(
+              ContentTypes.`text/plain(UTF-8)`,
+              source.map(str => ByteString(str, "UTF8"))
+            )
+          )
+        }
+      }
+    }
+
+  val bindingFuture: Future[Http.ServerBinding] = Http().bindAndHandle(route, "localhost", 9001)
+  bindingFuture onComplete {
+    case Success(binding) => println(s"Server is listening on port: ${binding.localAddress.getPort}")
+    case Failure(ex) =>
+      println(s"Binding fails with error: ${ex.getMessage}")
+  }
+  StdIn.readLine()
+  system.terminate()
+}
