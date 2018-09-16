8713daf8d36fb56f389a7fe8bf8bd9373b172c08
kuanyu <starmap0312@gmail.com>
2018-09-16 20:48:43 +0800

    add more

--- src/main/scala/akka_http/client/Step2Scaffolding.scala
+++ src/main/scala/akka_http/client/Step2Scaffolding.scala
@@ -1,9 +1,10 @@
 package akka_http.client
 
+import akka.NotUsed
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Flow, Source}
 import akka.util.ByteString
 
 import scala.concurrent.Future
@@ -13,7 +14,8 @@ object Step2Scaffolding extends Scaffolding with App {
   def sourceFuture: Future[Source[String, Any]] = {
     Http().singleRequest(request).map { response =>
       response.entity.dataBytes. // Source[ByteString, Any]
-        map(_.utf8String) // Source[String, Any]
+        map(_.utf8String).       // Source[String, Any]
+        via(printFlow)           // Source[String, Any], via a Flow that prints & returns the string itself
     }
   }
 
@@ -30,5 +32,11 @@ object Step2Scaffolding extends Scaffolding with App {
         }
       }
     }
+  }
+
+  def printFlow: Flow[String, String, NotUsed] = Flow[String].map {
+    str =>
+      println(str.slice(1, 2) + "...") // a Flow that just prints part of the string
+      str
   }
 }
