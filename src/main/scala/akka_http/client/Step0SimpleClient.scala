66bd7b3e068da5d904613685b8084e95c1f249f0
kuanyu <starmap0312@gmail.com>
2018-10-10 19:12:51 +0800

    add more

--- src/main/scala/akka_http/client/Step0SimpleClient.scala
+++ src/main/scala/akka_http/client/Step0SimpleClient.scala
@@ -1,8 +1,8 @@
 package akka_http.client
 
-import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import akka.http.scaladsl.Http
+import akka.actor.ActorSystem        // the runtime for everything related to akka
+import akka.stream.ActorMaterializer // the runtime for akka stream, on which akka-http is built
+import akka.http.scaladsl.Http // the entry point for akka-http (if writing java, use java dsl instead)
 //import akka.http.scaladsl.coding.Gzip
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.stream.scaladsl.Source
@@ -11,6 +11,17 @@ import scala.concurrent.Future
 import scala.io.StdIn
 import scala.util.{Failure, Success}
 
+// Web application vs. Web service:
+// 1) Web application: ex. play framework, which gives lots of structure and features for web application development
+// 2) Web service: ex. akka-http, which gives lots of infrastructure to expose an existing service via HTTP
+// akka-http vs. spray
+// 1) akka-http is based on akka-stream (which deals with streaming in a nice way)
+// 2) spray is based on akka-actor
+// akka-http related modules
+// 1) akka-http: the important one, on top of akka-http-core
+// 2) akka-http-core: low-level implementation
+// 3) akka-http-testkit: for testing
+// 4) akka-http-spray-json and akka-http-xml: integration with serialization libraries
 object Step0SimpleClient extends App {
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
@@ -24,11 +35,13 @@ object Step0SimpleClient extends App {
       map(_.utf8String)        // Source[String, Any]: converting ByteString to (utf8) String
   }
   sourceFuture onComplete {
-    case Success(source) => source.runForeach(println)
+    case Success(source) =>
+      source.runForeach(println)
     case Failure(ex) =>
+      println("we have failure")
       ex.printStackTrace()
       system.terminate()
   }
   StdIn.readLine()
-  system.terminate()
+  //system.terminate()
 }
--- src/main/scala/akka_http/server/HelloWorld.scala
+++ src/main/scala/akka_http/server/HelloWorld.scala
@@ -14,24 +14,32 @@ object HelloWorld extends App {
   import system.dispatcher
   implicit val materializer = ActorMaterializer()
 
-  def handler(request: HttpRequest) = {
-    // ex1. always return "Hello World" regardless of the HttpRequest, i.e. curl http://localhost:9000
-    //Future.successful(HttpResponse(entity = "Hello World"))
-    // ex2. if request matches the following path, i.e. curl http://localhost:9000/abc
+  def handler(request: HttpRequest): Future[HttpResponse] = {
+    // we can handle the request asynchronously here, i.e. we don't need to return HttpResponse immediately
     request match {
+      // handle requests that match the following path, i.e. curl http://localhost:9000/abc
       case HttpRequest(HttpMethods.GET, Uri.Path("/abc"), _, _, _) =>
+        // 1) returns Success:
+        //    always return "Hello World" regardless of the HttpRequest
         Future.successful(HttpResponse(entity = "Hello World"))
+        // 2) returns Failure:
+        //    internal server error, sending 500 response: i.e. HTTP/1.1 500 Internal Server Error
+        //Future.failed(new Exception("an exception occurs"))
     }
   }
+
+  // 1) Http().bindAndHandleAsync([handler], [interface], [port]):
+  //    bind to the network address and specify a handler for it
+  //    a handler is a function from HttpRequest to Future[HttpResponse]
   Http().bindAndHandleAsync(handler, "localhost", 9000).
     onComplete {
       case Success(_) =>
         println("Server started on port 9000. Type Enter to terminate")
-        StdIn.readLine()
+        StdIn.readLine() // if the binding is successful, block the execution, i.e. not terminating the program
         system.terminate()
       case Failure(ex) =>
         println("Binding failed")
-        ex.printStackTrace()
+        ex.printStackTrace() // if the binding is failed, print out the exception & terminate the program
         system.terminate()
     }
 }
