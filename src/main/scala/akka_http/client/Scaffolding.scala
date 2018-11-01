809801d7c1f8666dd4f91ffa3543ce265188fdc0
kuanyu <starmap0312@gmail.com>
2018-11-01 16:06:48 +0800

    add more

--- src/main/scala/akka_http/client/Scaffolding.scala
+++ src/main/scala/akka_http/client/Scaffolding.scala
@@ -19,6 +19,9 @@ class Scaffolding {
 
   def runWebService(route: Route): Unit = {
     val bindingFuture: Future[Http.ServerBinding] = Http().bindAndHandle(route, "localhost", 9001)
+    // note: to enable remote access to your server, you need to bind your server to the external interface
+    //       ex. to simply bind to all interfaces, you can set the host/IP to 0.0.0.0
+    //       i.e. Http().bindAndHandle(route, "0.0.0.0", 9001)
     bindingFuture onComplete {
       case Success(binding) => println(s"Server is listening on port: ${binding.localAddress.getPort}")
       case Failure(ex) => println(s"Binding fails with error: ${ex.getMessage}")
