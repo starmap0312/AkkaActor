a713430a15ebcc829c7e515a282c29211309045a
kuanyu <starmap0312@gmail.com>
2018-12-02 20:37:13 +0800

    add more

---
+++ src/main/scala/akka_futures/AsyncMapExample.scala
@@ -0,0 +1,8 @@
+package akka_futures
+
+// a good guideline explaining mapAsync & async boundary:
+// https://blog.colinbreck.com/maximizing-throughput-for-akka-streams/
+
+object AsyncMapExample extends App {
+
+}
