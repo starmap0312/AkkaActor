e588097158507a41dd9f2532f65f138a154b1b44
kuanyu <kuanyu@oath.com>
2020-10-16 10:46:36 +0800

    more

--- src/main/scala/akka_stream/GraphStages.scala
+++ src/main/scala/akka_stream/GraphStages.scala
@@ -4,7 +4,7 @@ import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
 import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 import akka.stream._
-import akka.stream.stage.{GraphStage, GraphStageLogic, OutHandler}
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
 
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
@@ -24,7 +24,7 @@ object GraphStages extends App {
   implicit val dispatcher = system.dispatcher
   implicit val materializer = ActorMaterializer()
 
-  // 1) build a new Source that will emit numbers from 1 until it is cancelled
+  // 1) build a new custom Source that will emit numbers from 1 until it is cancelled
   // first, define the "interface" of our operator, i.e. shape
   class NumbersSource extends GraphStage[SourceShape[Int]] {
     // Define the (sole) output port of this stage
@@ -40,9 +40,18 @@ object GraphStages extends App {
         // This state is safe to access and modify from all the callbacks that are provided by GraphStageLogic and the registered handlers.
         private var counter = 1
 
-        setHandler(out, new OutHandler {
+        // Output port handler
+        setHandler(out, new OutHandler { // This handler has two callbacks: onPull() & onDownstreamFinish()
           override def onPull(): Unit = {
+            // onPull():
+            //   this is called when the output port is ready to emit the next element
+            //   push(out, elem) is now allowed to be called on this port.
+
             push(out, counter)
+            // push(out, elem):
+            //   this pushes an element to the output port, i.e. out Outlet
+            //   this is only possible after the port has been pulled by downstream.
+
             counter += 1
           }
         })
@@ -52,11 +61,50 @@ object GraphStages extends App {
 
   // use of the NumbersSource
   // A GraphStage is a proper Graph, just like what GraphDSL.create would return
-  val sourceGraph: Graph[SourceShape[Int], NotUsed] = new NumbersSource
+  val sourceGraph1: Graph[SourceShape[Int], NotUsed] = new NumbersSource
   // Create a Source from the Graph to access the DSL
-  val source1: Source[Int, NotUsed] = Source.fromGraph(sourceGraph)
+  val source1: Source[Int, NotUsed] = Source.fromGraph(sourceGraph1)
   val future1: Future[Int] = source1.take(10).runFold(0)(_ + _) // 1 + 2 + ... + 10 = 55
-  println(Await.result(future1, 3.seconds))
+  println(Await.result(future1, 3.seconds)) // print 55
+
+
+  // 2) build a new custom Sink that will emit numbers from 1 until it is cancelled
+  class StdoutSink extends GraphStage[SinkShape[Int]] {
+
+    val in: Inlet[Int] = Inlet("StdoutSink") // the ports of this operator
+    override val shape: SinkShape[Int] = SinkShape(in)
+
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+      new GraphStageLogic(shape) {
+
+        // note: this requests one element at the Sink startup! (that's why the stream starts to flaw once run?)
+        override def preStart(): Unit = pull(in)
+
+        setHandler(in, new InHandler { // This handler has three callbacks: onPush() & onUpstreamFinish() & onUpstreamFailure
+          override def onPush(): Unit = {
+            // onPush():
+            //  it is not mandatory to grab the element, but
+            //  if it is pulled while the element has not been grabbed it will drop the buffered element (discarded)
+
+            val element = grab(in)
+            // acquire this element that has been received during an onPush()
+            // it cannot be called again until the port is pushed again by the upstream
+
+            println(element)
+
+            pull(in)
+            // request a new (next) element from the input port
+            // this is only possible after the port has been pushed by upstream
+          }
+        })
+      }
+  }
+
+  val sinkGraph2: Graph[SinkShape[Int], NotUsed] = new StdoutSink
+  // Create a Source from the Graph to access the DSL
+  val sink2: Sink[Int, NotUsed] = Sink.fromGraph(sinkGraph2)
+
+  val matValue2: NotUsed = source1.take(5).runWith(sink2) // print 1, 2, 3, 4, 5
 
   system.terminate()
 }
