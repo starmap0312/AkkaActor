31e21d301464633438d9db226e0cf8cf0438857a
kuanyu <kuanyu@oath.com>
2020-10-26 16:09:17 +0800

    more

--- src/main/scala/akka_stream/Substreams.scala
+++ src/main/scala/akka_stream/Substreams.scala
@@ -3,7 +3,7 @@ package akka_stream
 import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Flow, RunnableGraph, Sink, Source, SubFlow}
+import akka.stream.scaladsl.{Flow, Keep, RunnableGraph, Sink, Source, SubFlow}
 
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
@@ -29,7 +29,13 @@ object Substreams extends App {
   // Fan-out operators
   // 3) Source.divertTo(sink, predicate)/Flow.divertTo(sink, predicate):
   //    Each upstream element will either be diverted to the given sink, or the downstream consumer according to the predicate function applied to the element
-  val source3: Source[Int, NotUsed] = Source(1 to 10).divertTo(Sink.foreach(x => println(s"diverted to sink: ${x}")), _ % 2 == 0) // diverted to sink: 2, 4, 6, 8
+  val source3: Source[Int, NotUsed] = Source(1 to 10).divertTo(Sink.foreach(x => println(s"diverted to sink: ${x}")), _ % 2 == 0) // diverted to sink: 2, 4, 6, 8, 10
   val maValue3: Future[Done] = source3.runWith(Sink.ignore)
   Await.result(maValue3, 2.second)
+
+  // 4) Source.divertToMat(sink, predicate)(Keep.right):
+  //    Each upstream element will either be diverted to the given sink, or the downstream consumer according to the predicate function applied to the element
+  val source4: Source[Int, (NotUsed, Future[Done])] = Source(1 to 10).divertToMat(Sink.foreach(x => println(s"diverted to sink: ${x}")), _ % 2 == 0)(Keep.both) // diverted to sink: 2, 4, 6, 8, 10
+  val maValue4: (NotUsed, Future[Done]) = source4.to(Sink.foreach(x => println(s"elements not diverted: ${x}"))).run() // elements not diverted: 1, 3, 5, 7, 9
+  Await.result(maValue4._2, 2.second)
 }
