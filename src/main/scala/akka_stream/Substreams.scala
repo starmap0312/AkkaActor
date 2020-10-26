bee614d3ef56f19999d7c44139f5efea79979515
kuanyu <kuanyu@oath.com>
2020-10-26 15:39:41 +0800

    more

--- src/main/scala/akka_stream/Substreams.scala
+++ src/main/scala/akka_stream/Substreams.scala
@@ -5,7 +5,8 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, RunnableGraph, Sink, Source, SubFlow}
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 
 // ref: https://doc.akka.io/docs/akka/current/stream/stream-substream.html
 object Substreams extends App {
@@ -18,10 +19,17 @@ object Substreams extends App {
   val runnable1: RunnableGraph[NotUsed] = Source(1 to 10).groupBy(3, _ % 3).to(Sink.ignore)
   val maValue1: NotUsed = runnable1.run()
 
-  val source2: Source[Int, NotUsed] = Source(1 to 10).groupBy(3, _ % 3).mergeSubstreams
+  val source12: Source[Int, NotUsed] = Source(1 to 10).groupBy(3, _ % 3).mergeSubstreams
 
   // Flattening operators
   // 2) flatMapConcat():
-  val source3: Source[Int, NotUsed] = Source(1 to 2).flatMapConcat(i => Source(List.fill(3)(i)))
-  val maValue3: Future[Done] = source3.via(Flow[Int].map({ x => println(s"$x"); x })).runWith(Sink.ignore) // 1 1 1 2 2 2
+  val source2: Source[Int, NotUsed] = Source(1 to 2).flatMapConcat(i => Source(List.fill(3)(i)))
+  val maValue2: Future[Done] = source2.via(Flow[Int].map({ x => println(s"$x"); x })).runWith(Sink.ignore) // 1 1 1 2 2 2
+
+  // Fan-out operators
+  // 3) Source.divertTo(sink, predicate)/Flow.divertTo(sink, predicate):
+  //    Each upstream element will either be diverted to the given sink, or the downstream consumer according to the predicate function applied to the element
+  val source3: Source[Int, NotUsed] = Source(1 to 10).divertTo(Sink.foreach(x => println(s"diverted to sink: ${x}")), _ % 2 == 0) // diverted to sink: 2, 4, 6, 8
+  val maValue3: Future[Done] = source3.runWith(Sink.ignore)
+  Await.result(maValue3, 2.second)
 }
