471a320b647469f37dbdc7a7b4eeb812a1df0aa7
kuanyu <kuanyu@oath.com>
2020-11-30 14:50:32 +0800

    more

--- src/main/scala/ultrabrew/UltrabrewBasics.scala
+++ src/main/scala/ultrabrew/UltrabrewBasics.scala
@@ -2,46 +2,47 @@ package ultrabrew
 
 import io.ultrabrew.metrics.MetricRegistry
 import io.ultrabrew.metrics.reporters.SLF4JReporter
-import org.slf4j.LoggerFactory
-
-import scala.collection.JavaConverters._
+import org.slf4j.{Logger, LoggerFactory}
 
 class TestResource(metricRegistry: MetricRegistry) {
 
   val errorCounter = metricRegistry.counter("error") // Counter increment or decrement a 64-bit integer value
   val sizeGauge = metricRegistry.gauge("size") // Gauge measures a 64-bit integer value at given time
 
-  def handleError(value1: String, value2: String): Unit = {
-    errorCounter.inc("tag1", value1, "tag2", value1)
-    // .. do something ..
+  def countError(value1: String, value2: String): Unit = {
+    errorCounter.inc("error-tag1", value1, "error-tag2", value2)
   }
 
-  def handleSize(value: Long, tagList: Array[String]): Unit = {
+  def setSize(value: Long, tagList: Array[String]): Unit = {
     sizeGauge.set(value, tagList: _*)
-    // .. do something ..
   }
 }
 
 object UltrabrewBasics extends App {
 
-  val logger = LoggerFactory.getLogger(this.getClass)
+  val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
   // MetricRegistry
   //   it holds the configurations for metrics including metric name and type
   //   it also tracks and reports the metrics
   val metricRegistry: MetricRegistry = new MetricRegistry
   // SLF4JReporter: reports to SLF4J Logger with given name to log the aggregated values of the metrics
-  val reporter: SLF4JReporter = SLF4JReporter.builder().withName("metrics").build()
+  // note: need to add log4j2.xml configuration file in order to print the metrics to console
+  // withStepSize(): sets the reporting frequency, default=60.seconds
+  val reporter: SLF4JReporter = SLF4JReporter.builder().withName("metrics").withStepSize(1).build()
 
   metricRegistry.addReporter(reporter)
 
   val test = new TestResource(metricRegistry)
 
-  test.handleError("v1", "v2")
-  test.handleError("v1", "v2")
-
-  test.handleSize(1, Array("tag3", "v3"))
-  test.handleSize(2, Array("tag3", "v3"))
-
-  logger.debug("hello")
+  test.countError("v1", "v2") // metrics - lastUpdated=1606378223312 error-tag1=v1 error-tag2=v2 sum=1 error
+  Thread.sleep(1100)
+  test.countError("v1", "v2") // metrics - lastUpdated=1606378224322 error-tag1=v1 error-tag2=v2 sum=1 error
+  Thread.sleep(1100)
+  test.setSize(1, Array("size-tag1", "v3")) // metrics - lastUpdated=1606718786712 size-tag1=v3 count=1 sum=1 min=1 max=1 lastValue=1 size
+  Thread.sleep(1100)
+  test.setSize(2, Array("size-tag1", "v3")) // metrics - lastUpdated=1606718787720 size-tag1=v3 count=1 sum=2 min=2 max=2 lastValue=2 size
+  Thread.sleep(1100)
+  test.setSize(1, Array("size-tag1", "v3")) // metrics - lastUpdated=1606718787720 size-tag1=v3 count=1 sum=2 min=2 max=2 lastValue=1 size
+  Thread.sleep(1100)
 }
