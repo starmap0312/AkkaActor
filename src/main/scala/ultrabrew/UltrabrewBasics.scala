816161ab5bba75062824a73338e5806e2d1a18bb
kuanyu <kuanyu@oath.com>
2021-06-18 13:02:14 +0800

    more

--- src/main/scala/ultrabrew/UltrabrewBasics.scala
+++ src/main/scala/ultrabrew/UltrabrewBasics.scala
@@ -14,30 +14,30 @@ class TestResource(metricRegistry: MetricRegistry) {
   val errorCounter = metricRegistry.counter("error") // Counter increment or decrement a 64-bit integer value
 
   // Gauge: a type of metric whose value can increase or decrease over various measurements
-  //  it is stored in a 64-bit long, and it is reported as:
+  //  it is stored in a 64-bit long
+  val cacheSizeGauge = metricRegistry.gauge("cache-size") // Gauge measures a 64-bit integer value at given time
+
+  // Timer: measure elapsed time between two events, in nanoseconds, and act as counter for these events
   //  - Sum (e.g., total latency of request processing over default=60.seconds)
   //  - Count (e.g., number of requests processed over default=60.seconds)
   //  - Min (e.g., lowest latency across all requests over default=60.seconds)
   //  - Max (e.g., highest latency across all requests over default=60.seconds)
-  val latencyGauge = metricRegistry.gauge("latency") // Gauge measures a 64-bit integer value at given time
-
-  // Timer: measure elapsed time between two events, in nanoseconds, and act as counter for these events
-  val requestTimer = metricRegistry.timer("requests")
+  val latencyTimer = metricRegistry.timer("latency")
 
   def countError(tagList: Array[String]): Unit = {
     errorCounter.inc(tagList: _*)
   }
 
   def setSize(value: Long, tagList: Array[String]): Unit = {
-    latencyGauge.set(value, tagList: _*)
+    cacheSizeGauge.set(value, tagList: _*)
   }
 
   def handleRequest(tagList: Array[String]): Unit = {
-    val startTime = requestTimer.start()
+    val startTime = latencyTimer.start()
     // .. handle request ..
     Thread.sleep((Math.random() * 100).toLong) // random sleep [0, 100] ms
     // Note: no need for separate counter for requests per sec, as count is already included
-    requestTimer.stop(startTime, tagList: _*)
+    latencyTimer.stop(startTime, tagList: _*)
   }
 }
 
@@ -58,30 +58,30 @@ object UltrabrewBasics extends App {
 
   val test = new TestResource(metricRegistry)
 
-  test.countError(Array("error-tag1", "v1", "error-tag1", "v2"))
-  // 2020-11-30 18:03 INFO  metrics - lastUpdated=1606730603689 error-tag1=v1 error-tag1=v2 sum=1 error
-  test.countError(Array("error-tag1", "v1", "error-tag2", "v2"))
-  // 2020-11-30 18:03 INFO  metrics - lastUpdated=1606730603696 error-tag1=v1 error-tag2=v2 sum=1 error
-  test.setSize(1, Array("latency-tag1", "v3"))
-  test.setSize(2, Array("latency-tag1", "v3"))
-  test.setSize(1, Array("latency-tag1", "v3"))
-  // 2020-11-30 18:03 INFO  metrics - lastUpdated=1606730603699 latency-tag1=v3 count=3 sum=4 min=1 max=2 lastValue=1 latency
+  test.countError(Array("cache-error", "v1", "cache-error", "v2"))
+  // 2020-11-30 18:03 INFO  metrics - lastUpdated=1606730603689 cache-error=v1 cache-error=v2 sum=1 error
+  test.countError(Array("cache-error", "v1", "cache-error2", "v2"))
+  // 2020-11-30 18:03 INFO  metrics - lastUpdated=1606730603696 cache-error=v1 cache-error2=v2 sum=1 error
+  test.setSize(1, Array("cache-size", "v3"))
+  test.setSize(2, Array("cache-size", "v3"))
+  test.setSize(1, Array("cache-size", "v3"))
+  // 2020-11-30 18:03 INFO  metrics - lastUpdated=1606730603699 cache-size=v3 count=3 sum=4 min=1 max=2 lastValue=1 cache-size
 
   Thread.sleep(3000)
 
-  test.setSize(1, Array("latency-tag1", "v3"))
-  test.setSize(2, Array("latency-tag1", "v3"))
-  // 2020-11-30 18:07 INFO  metrics - lastUpdated=1606730820017 latency-tag1=v3 count=2 sum=3 min=1 max=2 lastValue=2 latency
+  test.setSize(1, Array("cache-size", "v3"))
+  test.setSize(2, Array("cache-size", "v3"))
+  // 2020-11-30 18:07 INFO  metrics - lastUpdated=1606730820017 cache-size=v3 count=2 sum=3 min=1 max=2 lastValue=2 cache-size
 
   Thread.sleep(3000)
 
-  test.handleRequest(Array("status", String.valueOf(200)))
-  test.handleRequest(Array("status", String.valueOf(400)))
-  test.handleRequest(Array("status", String.valueOf(500)))
-  test.handleRequest(Array("status", String.valueOf(200)))
-  // 2020-12-01 10:57 INFO  metrics - lastUpdated=1606791441577 status=200 count=2 sum=153287179 min=75395218 max=77891961 requests
-  // 2020-12-01 10:57 INFO  metrics - lastUpdated=1606791441428 status=400 count=1 sum=96555162 min=96555162 max=96555162 requests
-  // 2020-12-01 10:57 INFO  metrics - lastUpdated=1606791441502 status=500 count=1 sum=73397428 min=73397428 max=73397428 requests
+  test.handleRequest(Array("status", String.valueOf(200), "update-time", "v1"))
+  test.handleRequest(Array("status", String.valueOf(400), "update-time", "v1"))
+  test.handleRequest(Array("status", String.valueOf(500), "update-time", "v1"))
+  test.handleRequest(Array("status", String.valueOf(200), "update-time", "v1"))
+  // [metrics-1] INFO metrics - lastUpdated=1623992484316 status=200 update-time=v1 count=2 sum=51812394 min=9207269 max=42605125 latency
+  // [metrics-1] INFO metrics - lastUpdated=1623992484255 status=400 update-time=v1 count=1 sum=67725859 min=67725859 max=67725859 latency
+  // [metrics-1] INFO metrics - lastUpdated=1623992484307 status=500 update-time=v1 count=1 sum=51874143 min=51874143 max=51874143 latency
 
   Thread.sleep(3000)
 }
